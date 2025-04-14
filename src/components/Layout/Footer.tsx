d55968b9d3b1199aee1c4eea780b423e8149e927
Michael Whitt <michaelwhitt90@gmail.com>
2025-04-14 11:27:36 +0800

    change copyright

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -4,12 +4,13 @@ interface FooterProps {}
 
 const Footer = (props: FooterProps) => {
   const { title } = useGlobal()
+  const year = new Date()
 
   return (
     <footer className='bg-[#0a0a0a] text-white/60 py-8 mt-20'>
       <div className='container mx-auto px-4'>
         <div className='flex justify-between items-center'>
-          <p>&copy; 2024 Dev Portfolio. All rights reserved.</p>
+          <p>&copy; {year.getFullYear()} mdw</p>
           <div className='flex space-x-6'>
             <a href='#' className='hover:text-white transition-colors'>Privacy Policy</a>
             <a href='#' className='hover:text-white transition-colors'>Terms of Service</a>
